412fafac8642e02488d45db085a1145972fe42e8
Alfredo Dominguez <thecatura.ad@gmail.com>
2023-11-06 11:17:27 -0400

    Add Image Edit Functionality

--- Pages/Products/Edit.cshtml.cs
+++ Pages/Products/Edit.cshtml.cs
@@ -14,14 +14,18 @@ namespace FinalProject.Pages.Products
     public class EditModel : PageModel
     {
         private readonly FinalProject.Data.FinalProjectContext _context;
+        private readonly IWebHostEnvironment _env;
 
-        public EditModel(FinalProject.Data.FinalProjectContext context)
+        public EditModel(FinalProject.Data.FinalProjectContext context, IWebHostEnvironment env)
         {
             _context = context;
+            _env = env;
         }
 
         [BindProperty]
         public Product Product { get; set; } = default!;
+        [BindProperty] // This is the image file uploaded to the server which is optional
+        public IFormFile? ImageUpload { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -46,6 +50,21 @@ namespace FinalProject.Pages.Products
             if (!ModelState.IsValid)
             {
                 return Page();
+            }
+            if (ImageUpload != null)
+            {
+                Product.ImageName = ImageUpload.FileName ?? Product.ImageName;
+                // Save Image on server
+                DateTime now = DateTime.Now;
+                string time = now.ToString("yyyyMMddHHmmssffff");
+                string ImageFileName = time + "-" + Product.ImageName;
+                var filePath = Path.Combine(_env.ContentRootPath, "wwwroot", "images", ImageFileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await ImageUpload.CopyToAsync(stream);
+                }
+                Product.ImageName = ImageFileName;
             }
 
             _context.Attach(Product).State = EntityState.Modified;
