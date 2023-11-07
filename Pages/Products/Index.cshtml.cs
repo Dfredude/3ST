33217f988c084459aa7d71a587f1fd7d6771668c
Alfredo Dominguez <thecatura.ad@gmail.com>
2023-11-07 10:43:32 -0400

    Removed all access to cx to products

--- Pages/Products/Index.cshtml.cs
+++ Pages/Products/Index.cshtml.cs
@@ -8,9 +8,11 @@ using Microsoft.EntityFrameworkCore;
 using FinalProject.Data;
 using FinalProject.Models;
 using System.Text.Json;
+using Microsoft.AspNetCore.Authorization;
 
 namespace FinalProject.Pages.Products
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
         private readonly FinalProject.Data.FinalProjectContext _context;
